102298e73c0f411a8d5a580a9ddbd4386bdf2293
vicktor111 <85438133+vicktor111@users.noreply.github.com>
2022-06-03 13:04:47 +0300

    Create Attribute and method update

--- graphs.py
+++ graphs.py
@@ -16,10 +16,27 @@ def create_num_float(num):
         yield f" {a}.{b} "
 
 
+class Attribute:
+    def __init__(self, name, value, color):
+        self.name = name
+        self.value = value
+        self.color = color
+
+    def set_value(self, new_value):
+        if type(new_value) == type(int()):
+            self.value.append(new_value)
+        elif type(new_value) == type(list()) or type(tuple()):
+            for i in range(len(new_value)):
+                self.value.append(new_value[i])
+
+    def __repr__(self):
+        return f'name: {self.name}.\nvalue: {self.value}.\ncolor: {self.color}.'
+
+
 class Graph:
 
     def __init__(self, master, width, height, num_x, num_y,
-                 *atributes: list[dict[str, list[int]]],
+                 *attributes: list[Attribute],
                  type_graph="normal",
                  more_nums=False):
         self._x = 10
@@ -29,7 +46,7 @@ class Graph:
         self.more_nums = more_nums
         self._height = height
         self._width = width
-        self._atributes = atributes
+        self._attributes = attributes
         self.num_y = num_y
         self.num_x = num_x
         self._canvas = Canvas(master, bg="#ffffff",
@@ -80,13 +97,13 @@ class Graph:
     def create_rectangle(self):
         y0 = self._height + 10
         x0 = 10
-        range_1 = self._width / (len(self._atributes) + 1)
-        for i in range(len(self._atributes)):
+        range_1 = self._width / (len(self._attributes) + 1)
+        for i in range(len(self._attributes)):
             x = x0 + (range_1 * i)
             self._canvas.create_rectangle(x, y0, x + 30, y0 + 5,
-                                          fill=self._atributes[i]["color"])
+                                          fill=self._attributes[i].color)
             self._canvas.create_text(
-                x + 60, y0, text=self._atributes[i]["name"])
+                x + 60, y0, text=self._attributes[i].name)
 
     def create_marks(self):
         """Розміщення об'єктів на графіку."""
@@ -100,18 +117,18 @@ class Graph:
             [x0, y0])
         self.create_rectangle()
         # Візуалізація даних.
-        for atribute in self._atributes:
-            y_coordinate = atribute["value"]
+        for attribute in self._attributes:
+            y_coordinate = attribute.value
             if self.more_nums == False:
                 for i in range(len(y_coordinate) - 1):
                     if x0 + (self._range_1 * i) < length_line_x:
-                        # Накреслиння ліній.
+                        # Накреслення ліній.
                         self._canvas.create_line(
                             x0 + (self._range_1 * i),
                             y0 - (self._range_1 * y_coordinate[i]),
                             x0 + (self._range_1 * (i + 1)),
                             y0 - (self._range_1 * y_coordinate[i + 1]),
-                            fill=atribute["color"])
+                            fill=attribute.color)
                     else:
                         break
             else:
@@ -128,20 +145,23 @@ class Graph:
                             y - 2,
                             x + 2,
                             y + 2,
-                            fill=atribute["color"])
+                            fill=attribute.color)
                     else:
                         break
 
-    def create_graph(self):
+    def create_graph(self, special_command=None):
+        if special_command == "clear":
+            self._canvas = Canvas(self.master, bg="#ffffff",
+                                  width=self._width, height=self._height + 30)
         # Довжини кордиатних прямих.
         height = self._height - 20
         width = self._width - 10
         canvas = self._canvas
-        # Накреслиння кордиатної прямої y.
+        # Накреслення кордиатної прямої y.
         canvas.create_line(20, 10, 20, height, fill="black")
         canvas.create_line(20, 10, 25, 15)
         canvas.create_line(20, 10, 15, 15)
-        # Накреслиння кордиатної прямої x.
+        # Накреслення кордиатної прямої x.
         canvas.create_line(20, height, width, height, fill="black")
         canvas.create_line(width, height, width - 6, height - 6)
         canvas.create_line(width, height, width - 6, height + 6)
@@ -156,10 +176,19 @@ class Graph:
     def pack(self):
         self._canvas.place(x=10, y=20)
 
+    def update(self, *new_attribute, clear=False,  x=10, y=20):
+        special_command = ''
+        if clear:
+            self._canvas.destroy()
+            special_command = "clear"
+        self._attributes = new_attribute
+        self.create_graph(special_command)
+        self.place(x, y)
+
     def __repr__(self):
         text = ''
-        for atribute in self._atributes:
-            title = atribute["name"]
+        for attribute in self._attributes:
+            title = attribute.name
             text += f"{title}, "
         return f"""    atributes: {text[0:len(text) - 2]}.
     height: {self._height}.
@@ -171,16 +200,18 @@ class Graph:
 class Graph_Сomparable(Graph):
 
     def __init__(self, master, width, height, num_y,
-                 atributes: dict[str, list[int]]):
-        super().__init__(master, width, height, 0, num_y, atributes)
+                 attribute: Attribute):
+        super().__init__(master, width, height, 0, num_y, attribute)
 
     def create_marks(self):
-        atribute = self._atributes[0]
+        # Початкові кординати точок.
+        attribute = self._attributes[0]
         x0 = 20
         y0 = self._height - 20
         length_line_x = self._width - 30
         self.paint_scale_y([x0, y0])
-        y_coordinate = atribute["value"]
+        # Накреслення поріваняльних прямокутників.
+        y_coordinate = attribute.value
         x0 = 40
         for i in range(len(y_coordinate)):
             if x0 + (self._range_1 * i) < length_line_x:
@@ -191,11 +222,20 @@ class Graph_Сomparable(Graph):
                     y0,
                     x + 40,
                     y0 - (self._range_1 * y_coordinate[i]),
-                    fill=atribute["color"][i])
+                    fill=attribute.color[i])
+                self._canvas.create_text(x + 20, y0 + 10,
+                                         text=attribute.name[i])
             else:
                 break
 
+    def update(self, new_attribute, x=10, y=20):
+        self._canvas.destroy()
+        self._attributes = new_attribute
+        self.create_graph()
+        self.place(x, y)
+
     def paint_scale_y(self, locations):
+        # Значення для накреслення шкали.
         canvas = self._canvas
         length_line_y = self._height - 30
         self._range_1 = length_line_y / self.num_y
@@ -210,22 +250,3 @@ class Graph_Сomparable(Graph):
                 canvas.create_text(locations[0] - 10, y, text=f"{i}")
             else:
                 break
-
-
-if __name__ == '__main__':
-    import math
-    window = Tk()
-    window.geometry("900x900")
-    atribute_1 = {"name": "numbers",
-                  "value": [(math.sqrt(i)) for i in range(1, 90)],
-                  "color": "#3aaa34"}
-    graph = Graph(window, 1000, 500, 100, 30,
-                  atribute_1, more_nums=True)
-
-    atribute = {"names": ["manye", "car", "houses"],
-                "value": [3, 4, 2],
-                "color": ["red", "green", "yellow"]}
-    graph_2 = Graph_Сomparable(window, 1000, 200, 5, atribute)
-    graph_2.place(100, 100)
-    graph.pack()
-    window.mainloop()
